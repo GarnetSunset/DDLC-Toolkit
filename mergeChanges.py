f97feeaf4c4ee21678326ead1ec1a51433610f64
James <p1n3tr335@gmail.com>
2017-10-19 13:50:43 -0400

    Wow. Dont do that.

--- mergeChanges.py
+++ mergeChanges.py
@@ -5,8 +5,8 @@ owd = os.getcwd()
 
 if os.path.isdir(owd+"/edits/"):
     if not os.path.isdir(owd+"/DDLCModded/"):
-    os.makedirs(owd+"/DDLCModded/")
-    dir_util.copy_tree(owd+"/DDLC/",owd+"/DDLCModded/")
+        os.makedirs(owd+"/DDLCModded/")
+        dir_util.copy_tree(owd+"/DDLC/",owd+"/DDLCModded/")
 
     dir_util.copy_tree(owd+"/edits/",owd+"/DDLCModded/game/")
 
