f43374bfd6f2ec8aa52ce4d0f83e9989b03af6db
James <p1n3tr335@gmail.com>
2017-10-19 12:19:05 -0400

    Edits

--- mergeChanges.py
+++ mergeChanges.py
@@ -3,10 +3,15 @@ import os
 
 owd = os.getcwd()
 
-if not os.path.isdir(owd+"/DDLCModded/"):
+if os.path.isdir(owd+"/edits/"):
+    if not os.path.isdir(owd+"/DDLCModded/"):
     os.makedirs(owd+"/DDLCModded/")
     dir_util.copy_tree(owd+"/DDLC/",owd+"/DDLCModded/")
 
-dir_util.copy_tree(owd+"/edits/",owd+"/DDLCModded/game/")
+    dir_util.copy_tree(owd+"/edits/",owd+"/DDLCModded/game/")
 
-print("Done Merging!")
+    print("Done Merging!")    
+
+else:
+    os.makedirs(owd+"/edits/")
+    print("Go Make some edits dummy!")
