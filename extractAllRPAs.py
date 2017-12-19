bff442e6069daffca5ac7167184d4e3a9859f4f8
Kanielyochanan <32210799+DeveloperTJ@users.noreply.github.com>
2017-12-19 11:57:30 -0600

    Add "python" command and fix unrpaDir

--- extractAllRPAs.py
+++ extractAllRPAs.py
@@ -5,17 +5,17 @@ owd = os.getcwd()
 if sys.version_info <= (3, 0):
     unrpaDir = owd + "/unrpa2/unrpa.py"
 if sys.version_info >= (3, 0):
-    unrpaDir = owd + "/unrpa/unrpa.py"
+    unrpaDir = owd + "/unrpa/unrpa"
 
 if os.path.exists(owd + "/DDLC/game/audio.rpa"):
     os.chdir(owd + "/DDLC/game/")
-    os.system(unrpaDir + " " + owd + "/DDLC/game/audio.rpa")
+    os.system("python " + unrpaDir + " " + owd + "/DDLC/game/audio.rpa")
     os.remove(owd + "/DDLC/game/audio.rpa")
-    os.system(unrpaDir + " " + owd + "/DDLC/game/fonts.rpa")
+    os.system("python " + unrpaDir + " " + owd + "/DDLC/game/fonts.rpa")
     os.remove(owd + "/DDLC/game/fonts.rpa")
-    os.system(unrpaDir + " " + owd + "/DDLC/game/images.rpa")
+    os.system("python " + unrpaDir + " " + owd + "/DDLC/game/images.rpa")
     os.remove(owd + "/DDLC/game/images.rpa")
-    os.system(unrpaDir + " " + owd + "/DDLC/game/scripts.rpa")
+    os.system("python " + unrpaDir + " " + owd + "/DDLC/game/scripts.rpa")
     os.remove(owd + "/DDLC/game/scripts.rpa")
 else:
     print("Already Extracted Nerd!")
